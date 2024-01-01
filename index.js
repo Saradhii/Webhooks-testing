ec183ca00ff0836429d755e48ae10d6a8e37ad63
Saradhi <99667252+Saradhii@users.noreply.github.com>
2024-01-01 16:31:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.post('/webhook', (req, res) => {
   // extract the courier data from the webhook notification payload
   // const { data } = req.body;
   // res.send("data",data);
+    
   // store the courier data in a database or send it to the frontend
   // using a WebSocket or a REST API endpoint
 });
